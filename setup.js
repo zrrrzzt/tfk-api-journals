c3928e690c712befa318bc5d39458a6c90e0ec12
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-04-08 14:12:10 +0200

    Initial commit

--- setup.js
+++ setup.js
@@ -9,12 +9,24 @@ var textIndexFields = {
   'SA_OFFTITTEL': 'text',
   'JOURNPOST_OJ.JP_OFFINNHOLD': 'text'
 };
+var setupCounter = 0;
+var setupJobs = 3;
+
+function isSetupFinished(){
+  setupCounter++;
+  if (setupCounter === setupJobs) {
+    console.log('Setup finished!');
+    process.exit();
+  }
+}
 
 function handleCallback(error, data) {
   if (error) {
     console.error(error);
   } else {
+    console.log('Action performed');
     console.log(data);
+    isSetupFinished();
   }
 }
 
@@ -32,13 +44,13 @@ function addDocument(options, callback) {
   });
 }
 
-//db.createCollection('journals', handleCallback);
-
 journals.ensureIndex(textIndexFields, {'default_language': 'nb'}, function(err, data){
   if (err) {
     console.error(err);
   } else {
+    console.log('TextIndex OK');
     console.log(data)
+    isSetupFinished();
   }
 });
 
@@ -46,7 +58,9 @@ journals.ensureIndex({'JOURNPOST_OJ.JP_JDATO': 1}, function(err, data){
   if (err) {
     console.error(err);
   } else {
-    console.log(data)
+    console.log('Index OK');
+    console.log(data);
+    isSetupFinished();
   }
 });
 
